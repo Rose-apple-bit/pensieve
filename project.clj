e7ebe39183b57a92ebe88c93ce1d74f1564887d1
Shane Mulligan <mullikine@gmail.com>
2022-01-19 14:58:03 +1300

    Wed 19 Jan 2022 14:58:03

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-http "3.9.1"]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [cheshire "5.8.1"]
+                 [org.clojure/tools.reader "1.3.2"]
+                 [com.github.serceman/jnr-fuse "0.5.2.1"]]
   :main ^:skip-aot pensieve.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
