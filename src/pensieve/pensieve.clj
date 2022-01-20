8cc9154bd407f84319daab13971e3c476f4e6412
Shane Mulligan <mullikine@gmail.com>
2022-01-20 22:50:49 +1300

    20.01.22

--- src/pensieve/pensieve.clj
+++ src/pensieve/pensieve.clj
@@ -42,11 +42,11 @@
 (def mapi-get-pensieve-directories (memoize api-get-pensieve-directories))
 
 ;; Pull some remote values
-(defn api-get-pensieve-filenames [filename]
+(defn api-get-pensieve-filenames [directory]
   (comment
     (-> (client/get
          ;; "https://dog.ceo/api/breeds/list/all"
-         (str "https://dog.ceo/api/breed/" filename "/images")
+         (str "https://dog.ceo/api/breed/" directory "/images")
          {:as :json})
         :body :message))
   (map
@@ -54,7 +54,7 @@
    (json/decode
     (penf
      "pf-list-subdirectories/1"
-     (str "/dumbledores_adventures/" filename "/")))))
+     (str "/dumbledores_adventures/" directory "/")))))
 ;; This gets the body and then gets the message from the body
 
 (def mapi-get-pensieve-filenames (memoize api-get-pensieve-filenames))
@@ -62,16 +62,16 @@
 ;; How does the threading macro work with a :body key as the first form?
 ;; I think it converts :body to (:body).
 ;; So this is syntax sugar, for then extracting the value associated with the key.
-(defn api-get-pensieve-file [filename s]
-  (-> (client/get
-       (str "https://images.dog.ceo/breeds/" filename "/" s)
+(defn api-get-pensieve-file [directory s]
+  (comment (-> (client/get
+       (str "https://images.dog.ceo/breeds/" directory "/" s)
        ;; {:as :stream}
        {:as :byte-array})
-      :body)
+      :body))
   (penf
    "pf-generate-the-contents-of-a-new-file/6"
    ""
-   filename
+   directory
    "/dumbledores_adventures/"
    ;; ls (other files)
    ""
@@ -80,25 +80,25 @@
    ;; ls dirs here
    ""))
 ;; So it might as well be written like this:
-;; (defn api-get-pensieve-file [filename s]
+;; (defn api-get-pensieve-file [directory s]
 ;;   (:body (client/get
-;;           (str "https://images.dog.ceo/breeds/" filename "/" s)
+;;           (str "https://images.dog.ceo/breeds/" directory "/" s)
 ;;           ;; {:as :stream}
 ;;           {:as :byte-array})))
 
 (def mapi-get-pensieve-file (memoize api-get-pensieve-file))
 
-(defn get-pensieve-filenames [filename]
-  (mapi-get-pensieve-filenames filename))
+(defn get-pensieve-filenames [directory]
+  (mapi-get-pensieve-filenames directory))
 
 (defn get-pensieve-file
   "Ensure that P has the leading slash.
   Sample: /whippet/n02091134_10242.jpg"
   [p]
-  (let [[_ filename s] (u/split-by-slash p)]
-    (mapi-get-pensieve-file filename s)))
+  (let [[_ directory s] (u/split-by-slash p)]
+    (mapi-get-pensieve-file directory s)))
 
-(defn get-pensieve-filenames []
+(defn get-pensieve-directories []
   (mapi-get-pensieve-directories)
   ;; (->> (mapi-get-pensieve-directories)
   ;;      keys
@@ -106,43 +106,43 @@
   ;;      (into []))
   )
 
-(def filenames-atom (atom nil))
+(def directories-atom (atom nil))
 
-(defn set-filenames-atom! []
-  (reset! filenames-atom (get-pensieve-filenames)))
+(defn set-directories-atom! []
+  (reset! directories-atom (get-pensieve-directories)))
 
-(defn get-files []
-  (if @filenames-atom @filenames-atom
-      (set-filenames-atom!)))
+(defn get-directories []
+  (if @filenames-atom @directories-atom
+      (set-directories-atom!)))
 
-(defn filename-exists? [path]
-  (u/member (subs path 1) (get-files)))
+(defn directory-exists? [path]
+  (u/member (subs path 1) (get-directories)))
 
-(defn get-few-pensieve-filenames [filename]
-  (into [] (take 10 (get-pensieve-filenames filename))))
+(defn get-few-pensieve-filenames [directory]
+  (into [] (take 10 (get-pensieve-directories directory))))
 
 (defn get-filename-only [s]
   (nth (reverse (u/split-by-slash s)) 0))
 
-(defn get-file-list-clean [filename]
+(defn get-file-list-clean [directory]
   (doall
-   (into [] (map get-filename-only (get-few-pensieve-filenames filename)))))
+   (into [] (map get-filename-only (get-few-pensieve-filenames directory)))))
 
 (def file-listing-cache (atom {}))
 
-(defn set-file-listing-cache! [filename]
-  (swap! file-listing-cache conj {(keyword filename) (get-file-list-clean filename)}))
+(defn set-file-listing-cache! [directory]
+  (swap! file-listing-cache conj {(keyword directory) (get-file-list-clean directory)}))
 
-(defn get-pensieve-list! [filename]
-  (let [kw (keyword filename)]
+(defn get-pensieve-list! [directory]
+  (let [kw (keyword directory)]
     (if (kw @file-listing-cache)
       (kw @file-listing-cache)
-      (kw (set-file-listing-cache! filename)))))
+      (kw (set-file-listing-cache! directory)))))
 
 (defn file-exists?
   "Check against the path string, S always has a leading slash.
   Sample: /whippet/n02091134_10918.jpg"
   [p]
-  (let [[_ filename s] (u/split-by-slash p)]
-    (let [files ((keyword filename) @file-listing-cache)]
+  (let [[_ directory s] (u/split-by-slash p)]
+    (let [files ((keyword directory) @file-listing-cache)]
       (u/member s files))))
